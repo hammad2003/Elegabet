869ca38697fe3b61b396c2409a62b5784a57a68c
hammad2003 <hmuhammad@elpuig.xeill.net>
2024-02-20 02:49:24 +0100

    ELEGABET

---
+++ app/src/main/java/com/example/elegabet/ChargingScreenFragment.java
@@ -0,0 +1,64 @@
+package com.example.elegabet;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChargingScreenFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChargingScreenFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ChargingScreenFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ChargingScreenFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ChargingScreenFragment newInstance(String param1, String param2) {
+        ChargingScreenFragment fragment = new ChargingScreenFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_charging_screen, container, false);
+    }
+}
